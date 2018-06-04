518ef5bcd9fc01d79cf1e78f6c9d33be73aac2fb
DaniPeralta <dani.peralta.de@gmail.com>
2018-06-04 19:45:37 +0200

    Add server HTTP

---
+++ src/main/scala/com/example/mytask/Server.scala
@@ -0,0 +1,26 @@
+package com.example.mytask
+
+import cats.effect.{Effect, IO}
+import com.example.mytask.http.Routes
+import fs2.{Stream, StreamApp}
+import fs2.StreamApp.ExitCode
+import org.http4s.server.blaze.BlazeBuilder
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Server extends HttpServer [IO] {
+
+}
+
+class HttpServer[F[_]: Effect] extends StreamApp[F] {
+
+  def service[F[_]: Effect] = new Routes[F].service
+
+  override def stream(args: List[String], requestShutdown: F[Unit]): Stream[F, ExitCode] =
+    BlazeBuilder[F]
+      .bindHttp()
+      .mountService(service, "/")
+      .serve
+
+
+}
---
+++ src/main/scala/com/example/mytask/http/Routes.scala
@@ -0,0 +1,15 @@
+package com.example.mytask.http
+
+import cats.effect.Effect
+import org.http4s.HttpService
+import org.http4s.dsl.Http4sDsl
+
+class Routes[F[_]: Effect] extends Http4sDsl[F]{
+
+  val service: HttpService[F] = HttpService[F]{
+
+    case GET -> Root => Ok("Hola")
+
+
+  }
+}
