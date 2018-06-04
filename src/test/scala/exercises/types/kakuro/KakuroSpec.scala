4f8e7495ff98519751ed798d7938552a3db3a4e1
DaniPeralta <dani.peralta.de@gmail.com>
2018-06-04 18:51:18 +0200

    Add more tests

--- src/test/scala/exercises/types/kakuro/KakuroSpec.scala
+++ src/test/scala/exercises/types/kakuro/KakuroSpec.scala
@@ -45,8 +45,6 @@ class KakuroSpec extends FlatSpec with Matchers {
 
   "Approach2" should "be able to find the list of combinations" in {
 
-
-
     val approach1 = new Kakuro
     val approach2 = new Kakuro
     val approach3 = new Kakuro
@@ -63,6 +61,58 @@ class KakuroSpec extends FlatSpec with Matchers {
     approach3.combinationsA(11,4) shouldBe List(List(1, 2, 3, 5))
 
     approach4.combinationsA(15, 3, Set(7, 2)) shouldBe List(List(2, 6, 7))
+  }
 
+  "Hand.solve" should "be able to find the solution when 2 compatible hands without secures are passed" in {
+
+    val space1 = Spaces(13, 4, Set(1))
+    val space2 = Spaces(24, 3)
+
+    Hand(space1, space2).solve shouldBe 7
+  }
+
+
+  it should "be able to find the solution when 2 compatible hands with secures are passed" in {
+
+    val space1 = Spaces(10, 2, Set(3))
+    val space2 = Spaces(24, 3, Set(9))
+
+    Hand(space1, space2).solve shouldBe 7
+  }
+
+  it should "be able to find the solution when 2 compatible hands with/without secures are passed" in {
+
+    val space1 = Spaces(12, 4, Set(3))
+    val space2 = Spaces(22, 3)
+    val hand = Hand(space1, space2)
+
+    hand.solve shouldBe 6
+  }
+
+
+  it should "return 0 when both spaces has no any possible intersection" in {
+
+    val space1 = Spaces(8, 3)
+    val space2 = Spaces(23, 3)
+    val hand = Hand(space1, space2)
+
+    hand.solve shouldBe 0
+  }
+
+  it should "return -1 when exist several possible solutions" in {
+
+    val space1 = Spaces(12, 2)
+    val space2 = Spaces(23, 4)
+    val hand = Hand(space1, space2)
+
+    hand.solve shouldBe -1
+  }
+
+  it should "be able to find the unique solution when secures are restrictive" in {
+
+    val space1 = Spaces(19, 4, Set(5, 6))
+    val space2 = Spaces(16, 4, Set(7))
+
+    Hand(space1, space2).solve shouldBe 1
   }
 }
